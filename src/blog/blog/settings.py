985d7e99c7a084c9475c068b30ebf9252793cf52
NiniLiao <u9513031@cmu.edu.tw>
2020-12-02 01:43:56 +0800

    chore: update settings (dpend, deploy)

--- src/blog/blog/settings.py
+++ src/blog/blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,3 +184,8 @@ AUTHENTICATION_BACKENDS = (
 )
     
 SITE_ID = 1
+
+
+# Heroku部屬設定
+
+django_heroku.settings(locals())
